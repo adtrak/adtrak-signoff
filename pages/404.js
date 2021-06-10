6a9c2735c3c3c630f710ec0c8c0ab63573ca6ddc
Tom Nightingale <tom@weareleague.co.uk>
2021-06-10 11:23:28 +0100

    remove next link as all our links are external

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
 import Header from '../components/header'
-import Footer from '../components/footer'
 import { motion } from 'framer-motion'
 
 export default function ErrorPage() {
