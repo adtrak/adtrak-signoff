18e4c30761a26a21d0c729b7c2a41e7d50e16654
Tom Nightingale <tom@weareleague.co.uk>
2021-06-16 11:57:45 +0100

    fix bullet point sizing

--- components/nextStep.js
+++ components/nextStep.js
@@ -5,7 +5,7 @@ export default function nextStep({ step }) {
         <div className="w-full p-3 px-0 sm:p-3 sm:w-1/2 3xl:w-1/3">
             <div className="flex p-3 bg-black md:p-5 bg-opacity-10">
                 <Image src="/images/target.svg" alt="Bullet Point" width={30} height={30} />
-                <p className="px-4">{step}</p>
+                <p className="w-full px-4">{step}</p>
             </div>
         </div>
     )
