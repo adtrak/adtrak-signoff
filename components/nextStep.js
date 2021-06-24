693949d4ee8921b760ea9cf59453408d83216c9b
Tom Nightingale <tom@weareleague.co.uk>
2021-06-24 09:45:37 +0100

    fix nextStep styling

--- components/nextStep.js
+++ components/nextStep.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function nextStep({ step }) {
     return (
-        <div className="w-full p-3 px-0 sm:p-3 sm:w-1/2 3xl:w-1/3">
-            <div className="flex p-3 bg-black md:p-5 bg-opacity-10">
+        <div className="flex flex-wrap w-full p-3 px-0 sm:p-3 sm:w-1/2 3xl:w-1/3">
+            <div className="flex items-center w-full min-h-full p-3 bg-black md:p-5 bg-opacity-10">
                 <Image src="/images/target.svg" alt="Bullet Point" width={30} height={30} />
                 <p className="w-full px-4">{step}</p>
             </div>
